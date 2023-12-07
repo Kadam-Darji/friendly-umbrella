83221c0d6fd2bbf0b8f3570062608c84c28754d1
Kadam-Darji <202001198@daiict.ac.in>
2023-12-07 18:44:26 +0530

    Updated headlessness

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import puppeteer from 'puppeteer';
 jest.setTimeout(10000);
 test('renders learn react link', async() => {
   render(<App />);
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless:true});
   const page = await browser.newPage();
   await page.goto("https://kadam-darji.github.io/friendly-umbrella");
   const image = await page.screenshot();
