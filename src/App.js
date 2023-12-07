0ad717c8ce5e6ec914120ed93215677ae176e625
Kadam-Darji <202001198@daiict.ac.in>
2023-12-07 17:06:11 +0530

    Update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>A simple page for project!</p>
+      <br/><p>Testing!</p>''
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import App from './App';
+import puppeteer from 'puppeteer';
+import packageJson from '../package.json';
+
+test('renders learn react link', async() => {
+  render(<App />);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(packageJson.homepage);
+  const image = await page.screenshot(); 
+  expect(image).toMatchImageSnapshot();
+});
