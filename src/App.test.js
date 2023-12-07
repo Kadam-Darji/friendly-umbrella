4885d1c8c98b12e769a85317aa7dba3f7db4edf0
Kadam-Darji <202001198@daiict.ac.in>
2023-12-07 19:42:06 +0530

    Change in capture modified

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,16 @@ import puppeteer from 'puppeteer';
 jest.setTimeout(10000);
 test('renders learn react link', async() => {
   render(<App />);
-  const browser = await puppeteer.launch({headless:true});
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      `--no-sandbox`
+      `--disable-setuid-sandbox`
+      `--disable-extensions-except=${extensionPath}`,
+      `--load-extension=${extensionPath}`
+    ],
+    slowMo: 50
+  });
   const page = await browser.newPage();
   await page.goto("https://kadam-darji.github.io/friendly-umbrella");
   const image = await page.screenshot();
